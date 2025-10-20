620d78691fbada17836af02521adc5612fa7ee19
neilmandlik <neil.mandlik@imocha.co>
2025-10-20 19:20:11 +0530

    Changing and Finalizing the logic of making API calls and loading the data in a component

--- sem7-project/src/pages/TestComponent.jsx
+++ sem7-project/src/pages/TestComponent.jsx
@@ -1,14 +1,17 @@
 import { useSelector } from "react-redux"
 import store from "../store/store"
 import { getTestThunk } from "../store/test/testSlice"
+import { useLoaderData } from "react-router-dom"
+import { useEffect } from "react"
 
-export const testLoader = () => {
-    store.dispatch(getTestThunk())
+export const testLoader = async() => {
+    await store.dispatch(getTestThunk())
+    const state = store.getState()
+    return state.test
 }
 
 function TestComponent(){
-
-    const {message} = useSelector((state)=>state.test)
+    const {message} = useLoaderData()
 
     return(
         <>
